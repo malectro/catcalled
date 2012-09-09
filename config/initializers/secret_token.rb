5e67ff0e3a4202b7b7e944cf24b9309b3d74d207
Kyle Warren <qarren@gmail.com>
2012-09-09 12:22:50 -0400

    began using rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Catcalledr::Application.config.secret_token = 'e66b6be4508af06850a04f9b4161b76d9feda54ed2f3aa1f95476081cf1485f4abb4f9b3f9ff369875b61e6cfbaf6a124b2f409739249fc3f691aafb3dfb0117'
