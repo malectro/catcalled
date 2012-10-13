46936c1b15f3de0e833a26d5a7e992e3b6181da6
Kyle Warren <qarren@gmail.com>
2012-10-13 14:53:53 -0400

    did some more styling and pinned the header

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,6 +1,25 @@
 $(function () {
   var hash = location.hash.substr(1);
 
+  /* nav sticky */
+  var $nav = $('.cc-nav'),
+      navTop = $('.cc-nav').offset().top,
+      navStuck = false;
+
+  function checkNavHeight() {
+    var scrollTop = $(window).scrollTop();
+    if (scrollTop < navTop && navStuck) {
+      $nav.removeClass('stuck');
+      navStuck = false;
+    }
+    else if (scrollTop >= navTop && !navStuck) {
+      $nav.addClass('stuck');
+      navStuck = true;
+    }
+  }
+  checkNavHeight();
+  $(window).scroll(checkNavHeight);
+
   /* participant selection */
   var $links = $('.cc-participant-links a');
   $links.click(function () {
