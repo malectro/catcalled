9dfff489bcf6c040625e27fdc72ce3b3a313bd21
Kyle Warren <qarren@gmail.com>
2012-10-14 13:37:18 -0400

    added pagination to the participant page

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -21,18 +21,87 @@ $(function () {
   $(window).scroll(checkNavHeight);
 
   /* participant selection */
-  var $links = $('.cc-participant-links a');
-  $links.click(function () {
+  var $links = $('.cc-participant-entry-links a'),
+      $bioLink = $('.cc-bio-link'),
+      rightColWidth = $('.cc-col-r').width(),
+      currentEntry = 0;
+
+  function showBio() {
     $links.removeClass('selected');
-    $(this).addClass('selected');
-  });
-  if (location.hash) {
-    $links.removeClass('selected').each(function () {
-      if (this.href.search(location.hash) > -1) {
-        $(this).addClass('selected');
-      }
-    });
+    $bioLink.addClass('selected');
+
+    $('.cc-entry-book-wrap').fadeOut();
+    $('.cc-participant-bio').fadeIn();
   }
+
+  function showEntry(i) {
+    $('.cc-entry-book').animate({left: -i * rightColWidth}, 200);
+    currentEntry = i;
+
+    $bioLink.removeClass('selected');
+    $links.removeClass('selected');
+    $links.eq(i).addClass('selected');
+
+    $('.cc-participant-bio').fadeOut();
+    $('.cc-entry-book-wrap').fadeIn();
+  }
+
+  function showEntryByHash() {
+    var i = parseInt(location.hash.substr(7), 10);
+    showEntry(i);
+  }
+
+  function setEntryHash() {
+    location.hash = "#entry-" + currentEntry;
+  }
+
+  // hacky hash detection
+  $links.click(function () {
+    setTimeout(showEntryByHash, 0);
+  });
+  $bioLink.click(showBio);
+
+  if (location.hash && location.hash !== '#bio') {
+    showEntryByHash();
+  }
+
+  $('.cc-entry-book').css({width: $links.length * rightColWidth});
+
+  function pageLeft() {
+    if (location.hash !== '#bio' && currentEntry > 0) {
+      showEntry(currentEntry - 1);
+      setEntryHash();
+    }
+  }
+
+  function pageRight() {
+    if (location.hash !== '#bio' && currentEntry < $links.length - 1) {
+      showEntry(currentEntry + 1);
+      setEntryHash();
+    }
+  }
+
+  $('.cc-entry-prev').click(pageLeft);
+  $('.cc-entry-next').click(pageRight);
+
+
+  /* hot keys */
+  var hotKeys = {
+    //left
+    37: pageLeft,
+    //right
+    39: pageRight,
+    //j
+    74: pageRight,
+    //k
+    75: pageLeft
+  };
+  $(document.body).keydown(function (event) {
+    if (hotKeys[event.which]) {
+      hotKeys[event.which]();
+    }
+    console.log(event.which);
+  });
 
 });
 
